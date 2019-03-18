5e48dac85edfbd6849bd5412879578073d6194dc
uzairnz <uzairnz@hotmail.com>
2019-03-18 17:23:48 +0500

    Working

--- lib/manage_list.dart
+++ lib/manage_list.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
 
-
 // Createing Maunal list to view Tasks
-List <String> todo = ['Morning Run', 'Take Shower','Dress Up','Check Weather','Eat Healthy Breakfast'];
+List<String> todo = [
+  'Morning Run',
+  'Take Shower',
+  'Dress Up',
+  'Check Weather',
+  'Eat Healthy Breakfast'
+];
+
 class ManageList extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _ManageList();
-  } 
+  }
 }
 
 class _ManageList extends State<ManageList> {
-@override
+  @override
   Widget build(BuildContext context) {
-  return Column(children: todo.map((element)=>Card(
-    child: Column(
-      children: <Widget>[
-        Text(element,
-  textAlign: TextAlign.center,
-  overflow: TextOverflow.ellipsis,
-  style: TextStyle(fontWeight: FontWeight.bold),)
-      ],
-    ),
-  )).toList());   
-}
+    return Column(
+        children: todo
+            .map((element) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        element,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ))
+            .toList()
+            );
+  }
 }
